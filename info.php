a5d01d8ef6dcfb60b61919862df5738ac93194cf
ranuptdr <ranupatidar4589@gmail.com>
2024-05-07 16:29:07 +0530

    new

---
+++ info.php
@@ -0,0 +1,20 @@
+<?php //PHP Start Tag
+
+    echo 'Hello<br>';
+    echo("My name is Ranu<br>");
+    print 'all Code is executing at server side<br>';
+
+
+
+?> 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+        <h1>This is a Heading</h1>
+        <p>This is a paragraph.</p>
+    </body>
+</html>
